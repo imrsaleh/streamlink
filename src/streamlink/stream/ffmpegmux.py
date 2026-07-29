995e3b12b61c32222eb9c98923fd5460a8b73e07
imrsaleh <aburshrsh.90@gmail.com>
2026-07-30 02:06:42 +0300

    modified:   src/streamlink/stream/ffmpegmux.py

--- src/streamlink/stream/ffmpegmux.py
+++ src/streamlink/stream/ffmpegmux.py
@@ -220,11 +220,11 @@ class FFMPEGMuxer(StreamIO):
             ofmt
         ]
         
-        if dkeys:
-            self._cmd.extend(["-thread_queue_size", "32768"])
-            self._cmd.extend(["-decryption_keys", str(dkeys)])
+        
         for np in self.pipes:
-            
+            if dkeys:
+                self._cmd.extend(["-thread_queue_size", "32768"])
+                self._cmd.extend(["-decryption_keys", str(dkeys)])
 
             self._cmd.extend(["-i", str(np.path)])
 
