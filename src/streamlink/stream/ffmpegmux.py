b7236c119d63912e5292652c455d765c0b14c290
imrsaleh <aburshrsh.90@gmail.com>
2026-08-18 18:20:22 +0300

    modified:   src/streamlink/stream/ffmpegmux.py

--- src/streamlink/stream/ffmpegmux.py
+++ src/streamlink/stream/ffmpegmux.py
@@ -225,7 +225,7 @@ class FFMPEGMuxer(StreamIO):
                 self._cmd.extend([
                     "-thread_queue_size",
                     "32768",
-                    "-decryption_key",
+                    "-decryption_keys",
                     str(dkey)
                     ])
 
